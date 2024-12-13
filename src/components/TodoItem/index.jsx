fcc73ab463df5e9551b4cd9b2fb40c6262cc9623
duong-28 <thuyduong.kt.ftu@gmail.com>
2024-12-13 15:54:13 +1000

    index file fixed

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,12 +1,13 @@
 import './style.css'
 
 function TodoItem(props) {
+    const { todo,key, index, completeTodo} = props;
     // return <div className='todo'>{props.todo.text}</div>;
     return (
-        <div className={`todo ${props.todo.isCompleted ? "complete" : ""}`}>
-            {props.todo.text}
+        <div className={`todo ${todo.isCompleted ? "complete" : ""}`}>
+            {todo.text}
             <div>
-                <button onClick={() => props.completeTodo(props.index)}>Complete</button>
+                <button onClick={() => completeTodo(index)}>Complete</button>
             </div>
         </div>
     )
